a4a155eb7a36508b591913ce84eae9a6c9e99556
Md Habibur Rahman <shakibdshy@gmail.com>
2022-11-07 20:10:17 +0600

    gatsby plugin update

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,5 +8,16 @@ module.exports = {
   /* Your site config here */
   plugins: [
     'gatsby-plugin-postcss',
+    'gatsby-transformer-remark',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    'gatsby-transformer-json',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/src/posts/`,
+      },
+    },
   ],
 }
