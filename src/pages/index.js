8f8391741000080a463432833804bec4d37d5f28
Md Habibur Rahman <shakibdshy@gmail.com>
2022-11-07 18:51:44 +0600

    Initial File Setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,12 @@
 import React from "react"
+import Layout from "../components/Layout"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <>
+      <Layout>
+        <h2>Banner Section</h2>
+      </Layout>
+    </>
+  )
 }
